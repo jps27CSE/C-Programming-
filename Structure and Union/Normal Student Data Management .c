204c732e1046e70a5226ed2c95e24300dd224844
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2020-06-07 01:33:09 +0600

    Add files via upload

---
+++ Structure and Union/Normal Student Data Management .c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<string.h>
+
+struct nametype
+{
+    char first[20];
+    char last[20];
+
+};
+
+
+struct studenttype
+{
+   int id;
+
+   struct nametype name;
+};
+
+int main()
+{
+    struct studenttype student[5];
+
+    int i,n=5;
+
+    for(i=0;i<5;i++)
+    {
+        printf("Enter the ID for Student %d",i+1);
+        scanf("%d",&student[i].id);
+        printf("\nEnter the first name for student %d",i+1);
+        scanf("%s",student[i].name.first);
+        printf("\nEnter the last name for student %d",i+1);
+
+        scanf("%s",student[i].name.last);
+        printf("\n");
+
+
+    }
+
+    printf("Output : \n\n");
+
+    for(i=0;i<n;i++)
+    {
+        printf("ID: %d\n",student[i].id);
+        printf("Name: %s %s\n",student[i].name.first,student[i].name.last);
+    }
+
+
+}
